f617681401f8c72e1fad0817114650d2a06ec1c2
kai <kai.munekuni@gmail.com>
2019-10-27 16:12:12 +0900

    Revert "一覧画面のハンドラーを切り出した"

--- main.go
+++ main.go
@@ -31,8 +31,29 @@ func main() {
 	if err := initDB(db); err != nil {
 		panic(err)
 	}
-	h := handlers{}
-	http.HandleFunc("/", h.listPage())
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		resp, err := http.Get("https://lottery-dot-tenntenn-samples.appspot.com/available_lotteries")
+		if err != nil {
+			const status = http.StatusInternalServerError
+			http.Error(w, http.StatusText(status), status)
+			return
+		}
+		defer resp.Body.Close()
+
+		var lotteries []*Lottery
+		if err := json.NewDecoder(resp.Body).Decode(&lotteries); err != nil {
+			const status = http.StatusInternalServerError
+			http.Error(w, http.StatusText(status), status)
+			return
+		}
+
+		if err := listTmpl.Execute(w, lotteries); err != nil {
+			const status = http.StatusInternalServerError
+			http.Error(w, http.StatusText(status), status)
+			return
+		}
+	})
 
 	http.HandleFunc("/purchase_page", func(w http.ResponseWriter, r *http.Request) {
 		resp, err := http.Get("https://lottery-dot-tenntenn-samples.appspot.com/lottery?id=" + r.FormValue("id"))
