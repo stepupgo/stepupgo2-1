aad7836ca565bf3157b3d99536b980fa875fac47
Yang33 <kasai.yuta0810@gmail.com>
2019-10-27 16:17:22 +0900

    Split request handler in root

--- main.go
+++ main.go
@@ -21,6 +21,33 @@ import (
 	_ "github.com/mattn/go-sqlite3"
 )
 
+
+type PageHandler struct {
+}
+
+func (s *PageHandler) RootHandle (w http.ResponseWriter, r *http.Request){
+	resp, err := http.Get("https://lottery-dot-tenntenn-samples.appspot.com/available_lotteries")
+	if err != nil {
+		const status = http.StatusInternalServerError
+		http.Error(w, http.StatusText(status), status)
+		return
+	}
+	defer resp.Body.Close()
+
+	var lotteries []*Lottery
+	if err := json.NewDecoder(resp.Body).Decode(&lotteries); err != nil {
+		const status = http.StatusInternalServerError
+		http.Error(w, http.StatusText(status), status)
+		return
+	}
+
+	if err := listTmpl.Execute(w, lotteries); err != nil {
+		const status = http.StatusInternalServerError
+		http.Error(w, http.StatusText(status), status)
+		return
+	}
+}
+
 func main() {
 
 	db, err := sql.Open("sqlite3", "database.db")
@@ -31,29 +58,10 @@ func main() {
 	if err := initDB(db); err != nil {
 		panic(err)
 	}
+	
+	var page_handle_func PageHandler;
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		resp, err := http.Get("https://lottery-dot-tenntenn-samples.appspot.com/available_lotteries")
-		if err != nil {
-			const status = http.StatusInternalServerError
-			http.Error(w, http.StatusText(status), status)
-			return
-		}
-		defer resp.Body.Close()
-
-		var lotteries []*Lottery
-		if err := json.NewDecoder(resp.Body).Decode(&lotteries); err != nil {
-			const status = http.StatusInternalServerError
-			http.Error(w, http.StatusText(status), status)
-			return
-		}
-
-		if err := listTmpl.Execute(w, lotteries); err != nil {
-			const status = http.StatusInternalServerError
-			http.Error(w, http.StatusText(status), status)
-			return
-		}
-	})
+	http.HandleFunc("/", page_handle_func.RootHandle)
 
 	http.HandleFunc("/purchase_page", func(w http.ResponseWriter, r *http.Request) {
 		resp, err := http.Get("https://lottery-dot-tenntenn-samples.appspot.com/lottery?id=" + r.FormValue("id"))
