cb9b72ee0bcb78f261f58d1d6761ae8515f490c2
shirakiyo <tus.sk5678@gmail.com>
2019-10-27 16:26:18 +0900

    fix method

--- handler.go
+++ handler.go
@@ -7,29 +7,25 @@ import (
 
 type handler struct{}
 
-func (h *handler) getRoot() func(w http.ResponseWriter, r *http.Request) {
-	return func(w http.ResponseWriter, r *http.Request) {
-		{
-			resp, err := http.Get("https://lottery-dot-tenntenn-samples.appspot.com/available_lotteries")
-			if err != nil {
-				const status = http.StatusInternalServerError
-				http.Error(w, http.StatusText(status), status)
-				return
-			}
-			defer resp.Body.Close()
+func (h *handler) getRoot(w http.ResponseWriter, r *http.Request) {
+	resp, err := http.Get("https://lottery-dot-tenntenn-samples.appspot.com/available_lotteries")
+	if err != nil {
+		const status = http.StatusInternalServerError
+		http.Error(w, http.StatusText(status), status)
+		return
+	}
+	defer resp.Body.Close()
 
-			var lotteries []*Lottery
-			if err := json.NewDecoder(resp.Body).Decode(&lotteries); err != nil {
-				const status = http.StatusInternalServerError
-				http.Error(w, http.StatusText(status), status)
-				return
-			}
+	var lotteries []*Lottery
+	if err := json.NewDecoder(resp.Body).Decode(&lotteries); err != nil {
+		const status = http.StatusInternalServerError
+		http.Error(w, http.StatusText(status), status)
+		return
+	}
 
-			if err := listTmpl.Execute(w, lotteries); err != nil {
-				const status = http.StatusInternalServerError
-				http.Error(w, http.StatusText(status), status)
-				return
-			}
-		}
+	if err := listTmpl.Execute(w, lotteries); err != nil {
+		const status = http.StatusInternalServerError
+		http.Error(w, http.StatusText(status), status)
+		return
 	}
 }
