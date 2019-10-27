d8f9bc14cf068bffc0f9fa6f9f41330f662066d6
task4233 <tks.m1205@gmail.com>
2019-10-27 17:29:16 +0900

    [UPDATE] split purchase page handler

--- handler/http_handler.go
+++ handler/http_handler.go
@@ -34,3 +34,35 @@ func (h *Handler) HomeHandler(w http.ResponseWriter, r *http.Request) {
         return
     }
 }
+
+
+func (h *Handler) PurchasePageHandler(w http.ResponseWriter, r *http.Request) {
+    resp, err := http.Get("https://lottery-dot-tenntenn-samples.appspot.com/lottery?id=" + r.FormValue("id"))
+    if err != nil {
+        const status = http.StatusInternalServerError
+        http.Error(w, http.StatusText(status), status)
+        return
+    }
+    defer resp.Body.Close()
+
+    var l types.Lottery
+    if err := json.NewDecoder(resp.Body).Decode(&l); err != nil {
+        const status = http.StatusInternalServerError
+        http.Error(w, http.StatusText(status), status)
+        return
+    }
+
+    data := struct {
+        types.Lottery
+        Remain int64
+    }{
+        Lottery: l,
+        Remain:  l.Num, // TODO: 残りを計算する
+    }
+    if err := preview.PurchasePageTmpl.Execute(w, data); err != nil {
+        const status = http.StatusInternalServerError
+        http.Error(w, http.StatusText(status), status)
+        return
+    }
+}
+
