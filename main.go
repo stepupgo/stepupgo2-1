77dc4ab19a8449aee52f53625775429e5b6d46c8
Miyajima Kenta <shadowlink0122@gmail.com>
2019-10-27 17:03:31 +0900

    Main source code.

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+/*
+TODO
+	* テストコードを足す
+	* main関数を分ける
+	* 可読性
+	* テスタビリティ
+	* エラー処理（panicを使わない）
+*/
+
+import (
+	_ "github.com/mattn/go-sqlite3"
+	"github.com/stepupgo/stepupgo2-1/pkg/server"
+)
+
+func main() {
+	server.Run()
+}
