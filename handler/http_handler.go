424d73aef7eddd7f4986973ad6ff662dfbbcb292
task4233 <tks.m1205@gmail.com>
2019-10-27 17:31:26 +0900

    [UPDATE] split purchase handler

--- handler/http_handler.go
+++ handler/http_handler.go
@@ -66,3 +66,48 @@ func (h *Handler) PurchasePageHandler(w http.ResponseWriter, r *http.Request) {
     }
 }
 
+func (h *Handler) PurchaseHandler(w http.ResponseWriter, r *http.Request) {
+    id := r.FormValue("id")
+    num, err := strconv.Atoi(r.FormValue("num"))
+    if err != nil {
+        const status = http.StatusInternalServerError
+        http.Error(w, http.StatusText(status), status)
+        return
+    }
+    // TODO: パラメタのバリデーション
+
+    resp, err := http.Get("https://lottery-dot-tenntenn-samples.appspot.com/lottery?id=" + id)
+    if err != nil {
+        const status = http.StatusInternalServerError
+        http.Error(w, http.StatusText(status), status)
+        return
+    }
+    defer resp.Body.Close()
+
+    var l types.Lottery
+    if err := json.NewDecoder(resp.Body).Decode(&l); err != nil {
+        const status = http.StatusInternalServerError
+        http.Error(w, http.StatusText(status), status)
+        return
+    }
+
+    var count int
+    if err := db.QueryRow("SELECT COUNT(*) FROM Purchased WHERE lottery_id = ?", l.ID).Scan(&count); err != nil {
+        const status = http.StatusInternalServerError
+        http.Error(w, http.StatusText(status), status)
+        return
+    }
+
+    for i := 1; i <= num; i++ {
+        const sql = "INSERT INTO Purchased(lottery_id, number) values (?,?)"
+        format := fmt.Sprintf(`%%0%dd`, len(strconv.FormatInt(l.Num-1, 10)))
+        n := fmt.Sprintf(format, count+i)
+        if _, err := db.Exec(sql, id, n); err != nil {
+            const status = http.StatusInternalServerError
+            http.Error(w, http.StatusText(status), status)
+            return
+        }
+    }
+
+    http.Redirect(w, r, "/Purchase_page?id="+l.ID, http.StatusFound)
+}
--- main.go
+++ main.go
@@ -41,51 +41,7 @@ func main() {
 
 	http.HandleFunc("/Purchase_page", v.PurchasePageHandler)
 
-	http.HandleFunc("/Purchase", func(w http.ResponseWriter, r *http.Request) {
-		id := r.FormValue("id")
-		num, err := strconv.Atoi(r.FormValue("num"))
-		if err != nil {
-			const status = http.StatusInternalServerError
-			http.Error(w, http.StatusText(status), status)
-			return
-		}
-		// TODO: パラメタのバリデーション
-
-		resp, err := http.Get("https://lottery-dot-tenntenn-samples.appspot.com/lottery?id=" + id)
-		if err != nil {
-			const status = http.StatusInternalServerError
-			http.Error(w, http.StatusText(status), status)
-			return
-		}
-		defer resp.Body.Close()
-
-		var l types.Lottery
-		if err := json.NewDecoder(resp.Body).Decode(&l); err != nil {
-			const status = http.StatusInternalServerError
-			http.Error(w, http.StatusText(status), status)
-			return
-		}
-
-		var count int
-		if err := db.QueryRow("SELECT COUNT(*) FROM Purchased WHERE lottery_id = ?", l.ID).Scan(&count); err != nil {
-			const status = http.StatusInternalServerError
-			http.Error(w, http.StatusText(status), status)
-			return
-		}
-
-		for i := 1; i <= num; i++ {
-			const sql = "INSERT INTO Purchased(lottery_id, number) values (?,?)"
-			format := fmt.Sprintf(`%%0%dd`, len(strconv.FormatInt(l.Num-1, 10)))
-			n := fmt.Sprintf(format, count+i)
-			if _, err := db.Exec(sql, id, n); err != nil {
-				const status = http.StatusInternalServerError
-				http.Error(w, http.StatusText(status), status)
-				return
-			}
-		}
-
-		http.Redirect(w, r, "/Purchase_page?id="+l.ID, http.StatusFound)
-	})
+	http.HandleFunc("/Purchase", v.PurchaseHandler)
 
 	http.HandleFunc("/result", func(w http.ResponseWriter, r *http.Request) {
 		resp1, err := http.Get("https://lottery-dot-tenntenn-samples.appspot.com/result?id=" + r.FormValue("id"))
