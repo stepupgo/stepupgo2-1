cb20329becc3bdc73c607caf91f40f77ca01deb8
森下裕平 <justym8132@gmail.com>
2019-10-30 00:07:36 +0900

    main内からinitDBを分割

---
+++ dbhandler.go
@@ -0,0 +1,17 @@
+package main
+
+import "database/sql"
+
+func initDB(db *sql.DB) error {
+	const sql = `
+CREATE TABLE IF NOT EXISTS purchased (
+	lottery_id  TEXT NOT NULL,
+	number 		TEXT NOT NULL,
+	PRIMARY KEY(lottery_id, number)
+);
+`
+	if _, err := db.Exec(sql); err != nil {
+		return err
+	}
+	return nil
+}
